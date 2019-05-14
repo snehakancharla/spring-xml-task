81c79b3cb12c5fb7d07a108fc1b46508a06887a8
sneha <lakshmisneha.516@gmail.com>
2019-05-14 23:25:54 +0530

    completed second

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -8,13 +8,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
         public static void main(String ar[]) {
-            // Task-1 using Application context
-            ApplicationContext bean=new ClassPathXmlApplicationContext("beans.xml");
-            Movie movie =(Movie)bean.getBean("bean-1");
-            System.out.println(movie.getActor());
 
-            Movie movie1 =(Movie)bean.getBean("bean-2");
-            System.out.println(movie1.getActor());
+            ApplicationContext applicationContext3=new ClassPathXmlApplicationContext("constructor.xml");
+            Movie movie1=(Movie)applicationContext3.getBean("movie1");
+            System.out.println(movie1.toString());
 
 
 
