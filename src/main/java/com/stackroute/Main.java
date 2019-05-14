d8537022a4b80d1b456002b371f70e1e1b0f329d
sneha <lakshmisneha.516@gmail.com>
2019-05-15 00:03:50 +0530

    completed third

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -9,12 +9,14 @@ public class Main {
 
         public static void main(String ar[]) {
             // Task-1 using Application context
-            ApplicationContext bean=new ClassPathXmlApplicationContext("beans.xml");
-            Movie movie =(Movie)bean.getBean("bean-1");
-            System.out.println(movie.getActor());
+            ApplicationContext bean=new ClassPathXmlApplicationContext("constructorbean.xml");
+            /*Movie movie =(Movie)bean.getBean("beanbytype");
+            System.out.println(movie.getActor());*/
 
-            Movie movie1 =(Movie)bean.getBean("bean-2");
+
+            Movie movie1 =(Movie)bean.getBean("beanbyname");
             System.out.println(movie1.getActor());
+
 
 
 
